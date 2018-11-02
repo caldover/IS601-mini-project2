30fe1bd8402ea7a45d4822e6813d3fd2918dae5b
Christian Aldover <cka6@njit.edu>
2018-11-02 12:25:24 -0700

    3. Write a feature test to check if your about page returns a 200 status code

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -4,16 +4,26 @@ use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+    // IMPORTANT - use command: php artisan migrate:refresh --seed
+    // This will avoid the unique email violation when seeding
     public function run()
     {
-        //factory(App\User::class, 50)->create()->each(function ($u) {
-            //$u->posts()->save(factory(App\Post::class)->make());
-        //});
-        factory(App\User::class, 50)->create();
+        // Creates a total of 50 users:
+        // 1 defined admin user for FeatureTest purposes
+        // 49 randomly generated users
+
+        // Create defined admin user
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@email.com',
+            'email_verified_at' => now(),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+            'remember_token' => str_random(10),
+        ]);
+
+        // Create 49 random users
+        factory(App\User::class, 49)->create();
     }
 }
--- tests/Feature/FeatureTest.php
+++ tests/Feature/FeatureTest.php
@@ -15,14 +15,23 @@ class FeatureTest extends TestCase
     public function testRegisterPage()
     {
         $response = $this->get('/register');
-
         $response->assertStatus(200);
     }
 
     public function testLoginPage()
     {
         $response = $this->get('/login');
+        $response->assertStatus(200);
+    }
 
+    public function testAboutPage()
+    {
+        // Login with defined admin user from UsersTableSeeder
+        // If you don't log in then we will get 302 status instead due to redirect back to login page
+        $credentials = ['email'=>'admin@email.com', 'password'=>'secret'];
+        $this->call('POST', '/login', $credentials);
+
+        $response = $this->get('/about');
         $response->assertStatus(200);
     }
 }
