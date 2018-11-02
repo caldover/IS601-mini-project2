d1794cfa87fb5e94b03f1d8c6cc1e097c9b67263
Christian Aldover <cka6@njit.edu>
2018-11-02 11:08:10 -0700

    1. Write a feature test to check if the register page returns a 200 status code

--- tests/Unit/UnitTest.php
+++ tests/Unit/UnitTest.php
@@ -1,21 +1,20 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class UnitTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
+
     public function testBasicTest()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $this->assertTrue(true);
     }
 }
