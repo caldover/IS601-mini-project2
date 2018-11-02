eceb54c57f5395f7e4d2a5dcf243425b1f8f078a
Christian Aldover <cka6@njit.edu>
2018-11-02 15:57:00 -0700

    7. Create a unit test to delete a user in the database

--- tests/Unit/UnitTest.php
+++ tests/Unit/UnitTest.php
@@ -54,4 +54,32 @@ class UnitTest extends TestCase
         // Change the user's name back to its original name
         $randomUser->update(['name' => $initialName]);
     }
+
+    public function testDeleteUser()
+    {
+        // Get the initial count of users in the users table
+        $initialUsers = DB::table('users')->get()->toArray();
+        $initialCount = count($initialUsers);
+
+        // Create and insert new user
+        DB::table('users')->insert([
+            'name' => 'test',
+            'email' => 'test@email.com',
+            'email_verified_at' => now(),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+            'remember_token' => str_random(10),
+        ]);
+
+        // Remove the user from the users table
+        DB::table('users')->where('email', 'test@email.com')->delete();
+
+        // Get the new count of users in the users table
+        $newUsers = DB::table('users')->get()->toArray();
+        $newCount = count($newUsers);
+
+        // Check if initial count of users is equal to the new count
+        $this->assertSame($initialCount, $newCount);
+    }
 }
