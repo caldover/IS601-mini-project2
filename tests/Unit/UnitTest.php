976efaf3e4f9f533c6bdfa0330b18441788616dd
Christian Aldover <cka6@njit.edu>
2018-11-02 15:10:54 -0700

    5. Create a unit test to insert a user into the users table

--- tests/Unit/UnitTest.php
+++ tests/Unit/UnitTest.php
@@ -3,18 +3,36 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
 
 class UnitTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
 
-    public function testBasicTest()
+    public function testInsertUser()
     {
-        $this->assertTrue(true);
+        // Get the initial count of users in the users table
+        $initialUsers = DB::table('users')->get()->toArray();
+        $initialCount = count($initialUsers);
+
+        // Create and insert new user
+        DB::table('users')->insert([
+            'name' => 'test',
+            'email' => 'test@email.com',
+            'email_verified_at' => now(),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+            'remember_token' => str_random(10),
+        ]);
+
+        // Get the new count of users in the users table
+        $newUsers = DB::table('users')->get()->toArray();
+        $newCount = count($newUsers);
+
+        // Check if initial count of users + 1 is equal to the new count
+        $this->assertSame($initialCount + 1, $newCount);
+
+        // Remove the user from the users table
+        DB::table('users')->where('email', 'test@email.com')->delete();
     }
 }
