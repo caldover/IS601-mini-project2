0794bf0af0064d1dcbc21bdf1898f2aa3b77fcf5
Christian Aldover <cka6@njit.edu>
2018-11-02 17:11:26 -0700

    10. Create a unit test updating a car year to 2000

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -6,6 +6,6 @@ $factory->define(App\Car::class, function (Faker $faker) {
     return [
         'make' => $faker -> randomElement(["Ford", "Honda", "Toyota"]),
         'model' => $faker -> randomElement(["Model1", "Model2", "Model3"]),
-        'year' => $faker -> randomElement([2016, 2017, 2018])
+        'year' => (int)$faker -> randomElement([2016, 2017, 2018])
     ];
 });
--- tests/Unit/UnitTest.php
+++ tests/Unit/UnitTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Car;
 use Tests\TestCase;
 use Illuminate\Support\Facades\DB;
 
@@ -119,5 +120,24 @@ class UnitTest extends TestCase
 
         // Remove the car from the cars table
         DB::table('cars')->where('make', 'TestMake')->delete();
+    }
+
+    public function testUpdateCar()
+    {
+        // Select any user with IDs from 1 to 50
+        $randomID = rand(1, 50);
+
+        // Get the car, save its original year, and update its year to 2000
+        $randomCar = DB::table('cars')->where('id', $randomID);
+        $initialYear = $randomCar->get()[0]->year;
+        $randomCar->update(['year' => 2000]);
+
+        // Check to see if the randomly selected car's year was updated to 2000
+        $updatedCars = DB::table('cars')->where('year', '=', 2000)->get();
+        $updatedCar = $updatedCars[0];
+        $this->assertEquals($updatedCar->year, 2000);
+
+        // Change the car's year back to its original year
+        $randomCar->update(['year' => $initialYear]);
     }
 }
